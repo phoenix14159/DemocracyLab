71e5259e66b4e82bc54cadb133946aa72be6f42c
Bjorn Freeman-Benson <bjorn@newrelic.com>
2012-03-31 20:32:50 -0700

    allow user page to set/clear the admin flag on each user

---
+++ saveuseradmin_ajax.php
@@ -0,0 +1,13 @@
+<?php
+define('DL_BASESCRIPT',substr($_SERVER['SCRIPT_FILENAME'],0,strrpos($_SERVER['SCRIPT_FILENAME'],'/')));
+require_once(DL_BASESCRIPT . '/lib/lib.inc');
+
+if($democracylab_user_role == 0) {
+	exit;
+}
+
+$userid = pg_escape_string($_POST['userid']);
+$role = $_POST['admin'] == 'admin' ? 1 : 0;
+
+pg_query("UPDATE democracylab_users SET role = $role WHERE user_id = $userid");
+?>
--- users.php
+++ users.php
@@ -22,14 +22,29 @@ if($democracylab_user_role == 0) {
 	<div style="margin-left: -5px; background-image: url(images/dl.png); width: 293px; height: 102px; float: left; margin-right: 20px;"></div>
 </header>
 
+<script>
+function change_admin(userid,node) {
+	var x = node.value;
+	var data = {};
+	data['userid'] = userid;
+	data['admin'] = x;
+	$.ajax({
+		url: '<?= dl_facebook_url('saveuseradmin_ajax.php') ?>',
+		context: document.body,
+		data: data,
+		type: "POST",
+		global: false
+	});
+}
+</script>
 <section id="issue-section" class="clearfix">
 	<table style="color: black;">
 	<?php
-	$result = pg_query($dbconn, "SELECT * FROM democracylab_users");
+	$result = pg_query($dbconn, "SELECT * FROM democracylab_users ORDER BY name");
 	while($row = pg_fetch_object($result)) {
 		?><tr style="border: thin solid black;">
 			<td style="padding: 5px;"><?= htmlspecialchars($row->name) ?></td>
-			<td style="padding: 5px; color: grey;"><?= ($row->role == 1) ? 'admin' : '' ?></td>
+			<td style="padding: 5px; color: grey;"><select onchange="change_admin(<?= $row->user_id ?>,this);"><option value="admin" <?= $row->role == 1 ? 'selected' : '' ?>>admin</option><option value="user" <?= $row->role == 0 ? 'selected' : '' ?>>user</option></select></td>
 			<td style="padding: 5px; color: grey;"><?= $row->fb_id ?></td>
 		</tr>
 <?php
