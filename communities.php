ed259b1f3d6ef1ae13582dda6e80b89e9b86e86d
Bjorn Freeman-Benson <bjorn@newrelic.com>
2012-06-02 17:39:37 -0700

    add Google Analytics

--- communities.php
+++ communities.php
@@ -55,5 +55,17 @@ while($row = pg_fetch_object($result)) {
 	<p><a href="<?= dl_facebook_url('index.php') ?>">back to main page</a></p>
 	</div>
 </form>
-  </body>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-2879129-3']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>  </body>
 </html>
--- index.php
+++ index.php
@@ -335,5 +335,18 @@ $(function () {
 } );
 </script>
 <?php democracylab_hover_javascript(); ?>
-  </body>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-2879129-3']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+</body>
 </html>
