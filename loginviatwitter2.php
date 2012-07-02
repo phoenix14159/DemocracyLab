ba89907f17181dbdb2b52869ec259ae6cf0e27c5
Bjorn Freeman-Benson <bjorn@newrelic.com>
2012-07-02 12:05:43 -0700

    Login with Twitter.

--- loginviatwitter2.php
+++ loginviatwitter2.php
@@ -1,5 +1,6 @@
 <?php
 define('DL_BASESCRIPT',substr($_SERVER['SCRIPT_FILENAME'],0,strrpos($_SERVER['SCRIPT_FILENAME'],'/')));
+require_once(DL_BASESCRIPT . '/lib/prelib.php');
 
 /**
  * Take the user when they return from Twitter. Get access tokens.
